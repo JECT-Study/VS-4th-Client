a928a6e0a02e0a9ecdaddbdfaeba130e8a97f7d5
ohoCoding <youngho7482@gmail.com>
2026-04-30 09:22:28 +0900

    feat: :sparkles: add home route and login navigation

--- src/pages/routes/login.tsx
+++ src/pages/routes/login.tsx
@@ -1,9 +1,27 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/login")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/login"!</div>;
+  const navigate = useNavigate();
+
+  const handleClickLogin = () => {
+    navigate({ to: "/home" });
+  };
+
+  return (
+    <main className="flex flex-col items-center justify-center px-5 bg-white min-h-dvh">
+      <h1 className="text-2xl font-bold text-neutral-950">로그인</h1>
+
+      <button
+        type="button"
+        onClick={handleClickLogin}
+        className="w-full h-12 mt-8 text-sm font-bold text-white rounded-xl bg-neutral-950"
+      >
+        로그인하고 홈으로 이동
+      </button>
+    </main>
+  );
 }
