a6fcf6f85474174c89921dd8b2d883485cf0e8cc
최영광 <numeru@naver.com>
2026-05-27 21:54:09 +0900

    Update signup.tsx

--- src/pages/routes/signup.tsx
+++ src/pages/routes/signup.tsx
@@ -4,16 +4,6 @@ import { SignupPage } from "@features/signup/ui/SignupPage";
 import { createFileRoute, isRedirect, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/signup")({
-  beforeLoad: async ({ context: { queryClient } }) => {
-    let user: Awaited<ReturnType<typeof userQueryOptions>>["queryFn"] | null = null;
-    try {
-      user = (await queryClient.fetchQuery(userQueryOptions())) as typeof user;
-    } catch (err) {
-      if (isRedirect(err)) throw err;
-      return;
-    }
-    if (user) throw redirect({ to: "/home" });
-  },
   loader: ({ context: { queryClient } }) => queryClient.prefetchQuery(signupDefaultsQueryOptions()),
   component: SignupPage,
 });
