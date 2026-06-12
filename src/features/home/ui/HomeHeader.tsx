f61f5c1dc5c53a9ba185a390c0a0fba87e9d5110
ohoCoding <youngho7482@gmail.com>
2026-06-12 18:37:31 +0900

    fix: use unread notification icon in home header

--- src/features/home/ui/HomeHeader.tsx
+++ src/features/home/ui/HomeHeader.tsx
@@ -4,6 +4,8 @@ interface HomeHeaderProps {
 }
 
 export function HomeHeader({ hasUnreadNotification = false, onClickNotification }: HomeHeaderProps) {
+  const notificationIconSrc = hasUnreadNotification ? "/assets/icons/bell-undread.svg" : "/assets/icons/bell.svg";
+
   return (
     <header className="fixed top-0 z-20 flex items-center justify-between w-full max-w-md px-5 -translate-x-1/2 bg-white left-1/2 h-14">
       {/* 이미지 크기를 w-6 h-6으로 키우고, 하위 컨텐츠와 좌측 라인을 맞추기 위해 -ml-1.5 추가 */}
@@ -18,9 +20,7 @@ export function HomeHeader({ hasUnreadNotification = false, onClickNotification 
         onClick={onClickNotification}
         className="relative flex items-center justify-center h-9 w-9 -mr-1.5"
       >
-        <img src="/assets/icons/bell.svg" alt="알림" className="w-6 h-6" />
-
-        {hasUnreadNotification && <span className="absolute right-1.5 top-1.5 h-2 w-2 rounded-full bg-error" />}
+        <img src={notificationIconSrc} alt="알림" className="w-6 h-6" />
       </button>
     </header>
   );
